577de33408bd0d25fd47f94e0459826f9731b9fa
flaaviaa <flacorsi@gmail.com>
2025-04-30 12:03:07 -0300

    camera

--- API/ConsumoApi.py
+++ API/ConsumoApi.py
@@ -34,34 +34,38 @@ def count_confidence_ranges(data):
     
     return confidence_counts
 
-rf = Roboflow(api_key="sIfKQIjbxPJfJo1lu4Bc")
-project = rf.workspace().project("coffee-fruit-maturity-befkg")
-model = project.version(2).model
 
-root = Tk()
-root.withdraw()  
-image_path = askopenfilename(title="Escolha a imagem", filetypes=[("Imagens", "*.png;*.jpg;*.jpeg")])
+def gerarimagem(filename):
+	rf = Roboflow(api_key="sIfKQIjbxPJfJo1lu4Bc")
+	project = rf.workspace().project("coffee-fruit-maturity-befkg")
+	model = project.version(2).model
 
-if image_path:
-    result = model.predict(image_path).json()
+	root = Tk()
+	root.withdraw()  
+	image_path =filename
+	# askopenfilename(title="Escolha a imagem", filetypes=[("Imagens", "*.png;*.jpg;*.jpeg")])
+
+	if image_path:
+    		result = model.predict(image_path).json()
 
     
 
-    class_counts = count_classes(result)
-    confidence_counts = count_confidence_ranges(result)
+    		class_counts = count_classes(result)
+    		confidence_counts = count_confidence_ranges(result)
 
-    result['class_counts'] = class_counts
-    result['confidence_counts'] = confidence_counts
+                result['class_counts'] = class_counts
+    		result['confidence_counts'] = confidence_counts
 
-    output_directory = "api"
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+   	        output_directory = "api"
+    	 	if not os.path.exists(output_directory):
+        	 os.makedirs(output_directory)
 
-    output_file_path = os.path.join(output_directory, "resultado.json")
-    with open(output_file_path, 'w') as json_file:
-        json.dump(result, json_file, indent=4)
+    		output_file_path = os.path.join(output_directory, "resultado.json")
+                with open(output_file_path, 'w') as json_file:
+		        json.dump(result, json_file, indent=4)
 
-    draw_bounding_boxes_with_contours(image_path,output_file_path)
-    print(f"Resultado salvo em {output_file_path}")
-else:
-    print("Nenhuma imagem foi selecionada.")
+		    draw_bounding_boxes_with_contours(image_path,output_file_path)
+		    print(f"Resultado salvo em {output_file_path}")
+		else:
+		    print("Nenhuma imagem foi selecionada.")
+	
