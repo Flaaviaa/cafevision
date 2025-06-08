f7e272545eb707174308b08e51c96f67e14e1dfc
flaaviaa <flacorsi@gmail.com>
2025-06-08 20:07:41 -0300

    add-humidity-sensor

--- componentes/aht21.py
+++ componentes/aht21.py
@@ -7,12 +7,13 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
 def get_temperature():
-	tempemperature = sensor.temperature
+	temperature = sensor.temperature
+	print(f"Temperatura: {temperature:.2f}")
 	sleep(2)
-	print(f"cheguei")
 	return temperature
 
 def get_humidity():
 	humidity = sensor.relative_humidity
+	print(f"Umidade: {humidity:.2f}")
 	sleep(2)
 	return humidity
